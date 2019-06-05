775e72adee8338019d1f73a5b086da8fbbb52915
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-05 14:02:49 -0600

    rock paper scissors images work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import rock from "./images/rock.jpeg"
+import paper from "./images/paper.png"
+import scissors from "./images/scissors.jpeg"
+import { Container, Header, Button } from "semantic-ui-react";
+import OptionBox from "./OptionBox"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {userChoice: null, compChoice: null, showGame: false,}
+
+
+  toggleGame = () =>[
+    this.setState( state => {
+      return {showGame: !this.state.showGame}
+    })
+  ]
+  render() {
+    return (
+      <Container>
+        <Header as="h1">React Rock Paper Scissors</Header>
+        <Button onClick={this.toggleGame}>Let's Play</Button>
+        {this.state.showGame &&  <div className="game_div">
+
+        <OptionBox name = "rock" img={rock}/>
+        <OptionBox name = "paper" img={paper}/>
+        <OptionBox name = "scissors" img={scissors}/>
+       </div>}
+       
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/OptionBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Image,  } from "semantic-ui-react";
+
+const OptionBox = (props) =>(
+    <Image
+    src={props.img}
+    size="small"
+    />
+)
+
+
+
+export default OptionBox
